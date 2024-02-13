40d21eb574910ee353afbd7dbb8ecc1f6dbadcf1
RaoufJabriCamptocamp <raouf.jabri@camptocamp.com>
2024-02-13 11:33:38 +0100

    update

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
+"""Module flask."""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return render_template('index.html')
+    """Return index html file"""
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+@app.route("/submit", methods=["GET"])
+def form_submit():
+    "form veiw"
+    if request.method == "GET":
+        datas = request.form
+        print(datas)
+        return "Okay"
+
+    return "Not Okay"
+
+
+if __name__ == "__main__":
     app.run(port=4900, debug=True)
