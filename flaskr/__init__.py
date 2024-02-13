01c27e5b59ecbc38abbde851e459984e250d9e42
RaoufJabriCamptocamp <raouf.jabri@camptocamp.com>
2024-02-13 12:00:53 +0100

    add orm

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,25 +2,37 @@
 """Module flask."""
 
 from flask import Flask, render_template, request
+from flaskr.db import db_session, init_db
+from flaskr.models import User
 
 app = Flask(__name__)
+init_db()
 
 
 @app.route("/")
 def hello_world():
     """Return index html file"""
+    print(User.query.all())
     return render_template("index.html")
 
 
-@app.route("/submit", methods=["GET"])
+@app.route("/submit", methods=["POST"])
 def form_submit():
     "form veiw"
-    if request.method == "GET":
+    if request.method == "POST":
+        print(User.query.all())
         datas = request.form
-        print(datas)
+        user_data = User(datas["email1"], datas["password1"])
+        db_session.add(user_data)
+        db_session.commit()
         return "Okay"
-
     return "Not Okay"
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    """ Shitdown session """
+    db_session.remove()
 
 
 if __name__ == "__main__":
